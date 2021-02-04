b52787df39554c4803005a7ebfd9f9c8767b6f31
scorpka <ylltrabott@mail.ru>
2021-02-04 22:59:14 +0200

    v1.2 read and write from file

--- readFile.c
+++ readFile.c
@@ -1,16 +1,21 @@
 #include<stdio.h>
 int main(void) {
 FILE *fp;
- char a[20] = " ";
- char b[20] = " ";
-
-
+ char a;
+ char b;
+ char cac[90] = " ";
+   int n = 0;
   fp = fopen("readFile.txt","r");
-  fscanf(fp,"%s%s",&a,&b); 
+  for (int j=0; j<20; j++){
+    fscanf(fp,"%c%c",&a,&b);
+   if (a == EOF || b == EOF) {break;};
+   cac[n]= a; n++; cac[n] = b; n++;
+   
+  }
   fclose(fp);
  
   fp = fopen("readFile.txt","w");
-   fprintf(fp,"%s %s",&b,&a);
+   fprintf(fp,"%s",cac);
   fclose(fp); 
-   printf("%s%s",a,b); 
+   printf("%s",cac); 
 }
