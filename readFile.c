a1f8c05136d1a2a6efe1a9a1216b88c14feac930
scorpka <ylltrabott@mail.ru>
2021-02-06 00:33:12 +0200

    v1.3 added 2 functions read and write

--- readFile.c
+++ readFile.c
@@ -1,23 +1,35 @@
 #include<stdio.h>
-int main(void) {
-FILE *fp;
- char a;
- char b;
- char cac[90] = " ";
+  int readFile(char a,char b,FILE *fp,char *cac) {
    int n = 0;
-  fp = fopen("readFile.txt","r");
-  for (int j=0; j<20; j++){
-    fscanf(fp,"%c%c",&a,&b);
+   //extern char cac[90];
+   fp = fopen("readFile.txt","r");
+
+   for (int j=0; j<20; j++){
+   fscanf(fp,"%c%c",&a,&b);
    if (a == EOF || b == EOF) {break;};
    if  (a == ' ') { a= '\t';};
    if  (b == ' ') { b= '\t';};
    cac[n]= a; n++; cac[n] = b; n++;
-   
-  }
-  fclose(fp);
- 
-  fp = fopen("readFile.txt","w");
+   }
+
+   fclose(fp);
+}
+
+int writeFile(FILE *fp, char *cac) {
+   fp = fopen("readFile.txt","w");
    fprintf(fp,"%s",cac);
-  fclose(fp); 
-   printf("%s",cac); 
+   fclose(fp); 
+}
+
+int main(void) {
+ FILE *fp;
+ char a;
+ char b;
+ char cac[90] = " ";
+
+
+ readFile(a,b,fp,cac); 
+ writeFile(fp,cac);
+
+ printf("%s",cac); 
 }
